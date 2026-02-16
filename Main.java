59b371f2848e765017c6d6e7c8780eb8926f9ebd
Jorge <jquintanab04iesalbarregas@gmail.com>
2026-02-16 04:46:35 +0100

    Cuarta versión Ejercicio 3

--- Main.java
+++ Main.java
@@ -72,8 +72,7 @@ public class Main {
          */
 
 
-
-
+        /*
         //Ejercicio 2.
 
         int [] edades = new int [5];
@@ -128,9 +127,11 @@ public class Main {
         System.out.println("Hay " + mayorEdad + " personas mayores de edad.");
         System.out.println("La media de edad es " + edadMedia + " años.");
 
+         */
 
 
-        /*
+
+
         //Ejercicio 3.
 
         Random aleatorio = new Random();
@@ -170,7 +171,17 @@ public class Main {
                 System.out.println((i + 1) + ". " + numAleatorios[i]);
             }
         }
-         */
+
+
+        double mediaNumeros;
+        int acumulador = 0;
+        for (int i = 0; i < numAleatorios.length; i++) {
+            acumulador += numAleatorios[i];
+        }
+        mediaNumeros = (double) acumulador / numAleatorios.length;
+
+        System.out.println();
+        System.out.println("La media de los números generados es de: " + mediaNumeros);
 
     }
 }
